a6a1788858f1c9e167726aa57999314dea92ca27
DavidP001 <pp876314955@hotmail.com>
2018-11-20 14:17:26 +0800

    Host setting

--- wxServer/settings.py
+++ wxServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'yuv#ei)5tv5$dsh0tv&kee=v1cthx(s9+12fax)i2m6le6$t$='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
