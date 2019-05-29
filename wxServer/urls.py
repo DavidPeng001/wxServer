efcf7f1f6f2ad477b9402659cc9bd605bb698126
DavidP001 <pp876314955@hotmail.com>
2019-05-29 22:09:56 +0800

    Updata url route

--- wxServer/urls.py
+++ wxServer/urls.py
@@ -19,14 +19,19 @@ from api import views
 
 urlpatterns = [
     url(r'^api/search/', views.keyword_search_api),
-    url(r'^api/searchinfo', views.info_search_api),
+    url(r'^api/searchinfo/', views.info_search_api),
     url(r'^admin/', views.connection_test),
     url(r'^api/login/', views.login),
-    url(r'^api/captchaupdate', views.update_captcha),
-    url(r'^api/captchalogin', views.login_with_captcha),
+    url(r'^api/captchaupdate/', views.update_captcha),
+    url(r'^api/login/captcha/', views.login_with_captcha),
     url(r'^api/quicklogin/', views.quick_login),
     url(r'^api/roomsearch/', views.room_search),
     url(r'^api/roombooking/', views.room_booking),
+    url(r'^api/renew/',views.book_renew),
+    url(r'^api/renew/captcha/',views.book_renew_with_captcha),
+    url(r'^api/renewsearch/', views.book_renew_search),
+    url(r'^api/renewsearch/captcha/', views.book_renew_search_with_captcha),
+
     # warning: url mapping below should NOT be enabled in server
     # url(r'^api/crontest/', views.save_room_table)
 
